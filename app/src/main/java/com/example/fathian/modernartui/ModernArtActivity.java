dd9e0ad7d3c7e64a71fd2e74c83de4086368a5e8
Sogol Fathian <fathian@gmail.com>
2015-03-16 12:02:59 -0400

    layout complete
    completed the textview layout and colors

--- app/src/main/java/com/example/fathian/modernartui/ModernArtActivity.java
+++ app/src/main/java/com/example/fathian/modernartui/ModernArtActivity.java
@@ -30,7 +30,7 @@ public class ModernArtActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_more) {
             return true;
         }
 
